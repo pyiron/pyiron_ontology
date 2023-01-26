885e4deabcbde4d9eac37f637add029bf3c2e71b
liamhuber <liamhuber@greyhavensolutions.com>
2023-01-26 09:18:24 -0800

    Modify exposure

--- pyiron_ontology/__init__.py
+++ pyiron_ontology/__init__.py
@@ -5,16 +5,6 @@ del get_versions
 
 from pyiron_ontology.atomistics.constructor import AtomisticsOntology
 from pyiron_ontology.workflow import build_tree, build_path
-from pyiron_ontology.dynamic import DynamicOntologies
+from pyiron_ontology.dynamic import DynamicOntologies as dynamic
 
 from pyiron_ontology.atomistics.reasoning import AtomisticsReasoner
-
-
-class Pyironto:
-    build_tree = build_tree
-    dynamic = DynamicOntologies
-    static = None  # TODO: Allow loading constructed ontologies from a static/ dir
-
-# from pyiron_ontology import Pyironto
-# onto = Pyironto.dynamic.atomistics
-# Pyironto.build_tree(onto.Bulk_modulus)
