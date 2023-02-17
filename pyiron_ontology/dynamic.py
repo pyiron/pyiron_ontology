f87bcaa913be53072d49d1e1ae9fea006b319990
liamhuber <liamhuber@greyhavensolutions.com>
2023-02-17 15:25:44 -0800

    Give access to the example onto in the dynamic loader

--- pyiron_ontology/dynamic.py
+++ pyiron_ontology/dynamic.py
@@ -6,13 +6,21 @@ A class for lazy construction of the ontologies
 """
 
 from pyiron_ontology.atomistics.constructor import AtomisticsOntology
+from pyiron_ontology.example.constructor import ExampleOntology
 
 
 class DynamicOntologies:
     _atomistics = None
+    _example = None
 
     @classmethod
     def atomistics(cls):
         if cls._atomistics is None:
             cls._atomistics = AtomisticsOntology().onto
         return cls._atomistics
+
+    @classmethod
+    def example(cls):
+        if cls._example is None:
+            cls._example = ExampleOntology().onto
+        return cls._example
