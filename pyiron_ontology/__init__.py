ceaa35728c019ca0aa9a60a0422babedc2b8a26d
liamhuber <liamhuber@greyhavensolutions.com>
2023-02-22 12:19:30 -0800

    Expose the constructor at the top level

--- pyiron_ontology/__init__.py
+++ pyiron_ontology/__init__.py
@@ -5,6 +5,7 @@ del get_versions
 
 from pyiron_ontology.atomistics.constructor import AtomisticsOntology
 from pyiron_ontology.workflow import build_tree, build_path
+from pyiron_ontology.constructor import Constructor
 from pyiron_ontology.dynamic import DynamicOntologies as dynamic
 
 from pyiron_ontology.atomistics.reasoning import AtomisticsReasoner
