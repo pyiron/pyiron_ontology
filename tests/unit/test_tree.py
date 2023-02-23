af8336f60b9b345338cb5645c7346228eb66bb35
liamhuber <liamhuber@greyhavensolutions.com>
2023-02-23 15:33:09 -0800

    Fix test

--- tests/unit/test_tree.py
+++ tests/unit/test_tree.py
@@ -10,10 +10,10 @@ class TestTree(TestCase):
         # Don't take this test too seriously, it's not asserting a promised interface,
         # it's just making sure I can actually do stuff on the CI.
 
-        onto = pyiron_ontology.dynamic.atomistics()
-        tree = pyiron_ontology.build_tree(onto.Bulk_modulus)
+        onto = pyiron_ontology.dynamic.example()
+        tree = onto.output4_out.get_source_tree()
 
         while len(tree.children) > 0:
             tree = tree.children[0]
 
-        self.assertEqual(onto["CreateStructureBulk/output/structure"], tree.value)
+        self.assertEqual(onto.input2_inp, tree.value)
