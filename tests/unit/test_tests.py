af41e64a789d34d3633779bb25f535eb8a92e17f
liamhuber <liamhuber@greyhavensolutions.com>
2023-02-23 11:20:54 -0800

    Remove dummy integration test
    We have a real one now

--- tests/unit/test_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import pyiron_ontology
-
-
-class TestVersion(unittest.TestCase):
-    def test_version(self):
-        version = pyiron_ontology.__version__
-        print(version)
-        self.assertTrue(version.startswith('0'))
