f02c4361e6563e88ab3bde5da5296e9a95fb8e22
liamhuber <liamhuber@greyhavensolutions.com>
2023-04-20 13:33:45 -0700

    Explicitly pin sqlalchemy to 2.0.9
    [previously](https://github.com/pyiron/core/discussions/1#discussioncomment-5234174) this needed an explicit pin <2 to satisfy mendeleev. Now, however, it looks like they are [explicitly pinning 2.0.9](https://github.com/lmmentel/mendeleev/blob/04cfe2ce58734f18fefee658a44722ef49b0c7f9/poetry.lock#L3306), so I would be a bit surprised if this was the source of the trouble. However, previously it was a conda/pypi mismatch and they're using poetry (which I'm not so familiar with) so maybe such a mismatch persists?

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'pandas',
         'pint',
         'pyiron_atomistics>=0.2.63',
-        'sqlalchemy',
+        'sqlalchemy==2.0.9',
     ],
     cmdclass=versioneer.get_cmdclass(),
 
