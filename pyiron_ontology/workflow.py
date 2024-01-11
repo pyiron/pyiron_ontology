43e57167607af2ce57c6a803dc528b5ae9d3969a
liamhuber <liamhuber@greyhavensolutions.com>
2024-01-11 12:21:11 -0800

    Render stuff in alphabetical order by default
    Consistent ordering is important for testing examples, which need reliable output. Otherwise has no real input as it only impacts the render.

--- pyiron_ontology/workflow.py
+++ pyiron_ontology/workflow.py
@@ -5,6 +5,8 @@
 A tree structure for ontologically-informed workflows
 """
 
+from numpy import argsort
+
 
 class NodeTree:
     def __init__(self, value, parent=None):
@@ -14,8 +16,13 @@ class NodeTree:
         if parent is not None:
             parent.children.append(self)
 
-    def render(self, depth=0):
+    def render(self, depth=0, order_alphabetically=True):
         tabs = "".join(["\t"] * depth)
         print(f"{tabs}{self.value.name}")
-        for child in self.children:
+        children = (
+            [self.children[n] for n in argsort([str(c.value) for c in self.children])]
+            if order_alphabetically
+            else self.children
+        )
+        for child in children:
             child.render(depth=depth + 1)
